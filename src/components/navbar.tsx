deafe96073142d48efa803adde0aefe1e1828beb
kshetritej <tejghartikshetri@gmail.com>
2025-02-01 23:52:42 +0545

    chore(ui): input and button radius

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,42 @@
-import Image from "next/image";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import Link from "next/link";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Flag, Search } from "lucide-react";
 
 export default function Navbar() {
+const navItems = [
+    {
+        name: "Discover",
+        href: "/discover",
+    },
+    {
+        name: "Reviews",
+        href: "/reviews",
+    },
+    {
+        name: "More",
+        href: "/more",
+    }
+]
   return (
     <div className="bg-background p-4 flex justify-between">
         <div className="logo-and-search flex gap-2 items-center">
             <div className="logo font-bold text-lg">
-                <Image src="/thegreenflag_logo.png" width={80} height={80} alt="logo"/>
+                <Flag/>
             </div>
+            <div className="relative">
+            <Search className="absolute right-2 top-1 opacity-75"/>
             <Input placeholder="Search" />
+            </div>
         </div>
 
         <div className="menu-items">
-
+        {navItems.map((item, index) => (
+            <Link key={index} href={item.href}>
+                <Button variant="ghost">{item.name}</Button>
+            </Link>
+        ))}
         </div>
         <div className="login-and-signup flex gap-2 items-center">
             <SignedOut>

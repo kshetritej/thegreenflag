190acc8d81a272ab766e7aa771d6f16d9cdfdb45
kshetritej <tejghartikshetri@gmail.com>
2025-05-04 13:44:50 +0545

    fix: explore page search box default value issue, add all filter for category and clear search button

--- components/molecules/simple-filter-bar.tsx
+++ components/molecules/simple-filter-bar.tsx
@@ -1,12 +1,14 @@
 "use client"
 
-import {  Search } from "lucide-react"
+import { LucideX, Search } from "lucide-react"
 import { Badge } from "@/components/ui/badge"
 import { useEffect, useState } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
 import { Category } from "@prisma/client"
 import { Button } from "../ui/button"
 import { useDebounce } from "@/hooks/use-debounce"
+import { Input } from "../ui/input"
+import MyToolTip from "../atoms/MyTooltip"
 
 const MAIN_CATEGORIES = Object.values(Category).slice(0, 7)
 const OTHER_CATEGORIES = Object.values(Category).slice(7)
@@ -69,38 +71,59 @@ export default function SimpleFilterBar({ isHomepage = false }: { isHomepage: bo
   return (
     <div className="container mx-auto flex flex-col items-center justify-center gap-4 py-4">
       {!isHomepage && (
-   <div className="mt-8 w-full mx-auto">
+        <div className="mt-8 w-full mx-auto">
           <div className="relative flex items-center flex-col">
             <input
+              id="search-box"
               type="text"
               placeholder="What are you looking for?"
               className=" w-full px-4 py-3 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary"
               onChange={(e) => setSearchQuery(e.target.value)}
+              defaultValue={searchQuery}
               onKeyDown={(e) => {
                 if (e.key === "Enter") {
                   handleSearch(e)
                 }
               }}
             />
-
+            {
+              searchQuery.length > 0 &&
+              <MyToolTip content="Clear search">
+                <Button size={'icon'} className="rounded-full absolute right-28 top-[7px]"
+                  variant={'ghost'}
+                  onClick={() => {
+                    setSearchQuery("")
+                    // @ts-expect-error its not empty, i've defined it up in the input field
+                    document.getElementById("search-box").value = ""
+                  }}
+                ><LucideX /></Button>
+              </MyToolTip>
+            }
             <Button size={"default"} className="absolute right-0 top-0.4 h-[50px] rounded-full" onClick={handleSearch}>
-              <Search className="h-5 w-5" /> Search 
+              <Search className="h-5 w-5" /> Search
             </Button>
           </div>
         </div>
-    
+
       )}
 
       <div className="flex flex-wrap items-center gap-2">
         <div className="flex flex-wrap gap-2">
+          <Badge
+            variant={activeCategory === null ? "default" : "outline"}
+            className={`cursor-pointer p-2 min-w-[60px] ${activeCategory === null
+              ? "bg-primary text-primary-foreground hover:bg-primary/90"
+              : "bg-transparent"
+              }`}
+            onClick={() => setActiveCategory(null)}>All</Badge>
           {displayedCategories.map((category) => (
             <Badge
               key={category}
               variant={activeCategory === category ? "default" : "outline"}
-              className={`rounded-full cursor-pointer p-2 min-w-[60px] ${activeCategory === category
+              className={`cursor-pointer p-2 min-w-[60px] ${activeCategory === category
                 ? "bg-primary text-primary-foreground hover:bg-primary/90"
                 : "bg-transparent"
-              }`}
+                }`}
               onClick={() => handleCategoryClick(category)}
             >
               {category.replace(/_/g, " ")}
@@ -110,7 +133,7 @@ export default function SimpleFilterBar({ isHomepage = false }: { isHomepage: bo
           {MAIN_CATEGORIES.length < Object.values(Category).length && (
             <Badge
               variant="outline"
-              className="rounded-full cursor-pointer p-2"
+              className="bg-green-200 border-green-500  text-black cursor-pointer p-2"
               onClick={() => setShowAllCategories(!showAllCategories)}
             >
               {showAllCategories ? "Show Less" : "More..."}

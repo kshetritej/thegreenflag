14ec22cc6b732b9da8fd44a5d83564b8d0a55220
kshetritej <tejghartikshetri@gmail.com>
2025-05-22 07:10:01 +0545

    chore: minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,11 @@ import "./globals.css"
 import { Geist_Mono } from "next/font/google"
 import { EdgeStoreProvider } from "@/lib/edgestore"
 import { Toaster } from "@/components/ui/sonner"
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider, useSession } from "next-auth/react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ThemeProvider } from "@/components/theme-provider"
 import { TooltipProvider } from "@/components/ui/tooltip"
+import { useRouter } from "next/navigation"
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -30,19 +31,19 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className={` ${geistMono.variable} font-sans tracking-loose `}>
         <SessionProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
           >
             <TooltipProvider>
-          <QueryClientProvider client={queryClient}>
-          <EdgeStoreProvider>
-              {children}
-            <Toaster position="bottom-center" />
-            </EdgeStoreProvider>
-          </QueryClientProvider>
+              <QueryClientProvider client={queryClient}>
+                <EdgeStoreProvider>
+                  {children}
+                  <Toaster position="bottom-center" />
+                </EdgeStoreProvider>
+              </QueryClientProvider>
             </TooltipProvider>
           </ThemeProvider>
         </SessionProvider>
